b946c66e6086aef9d527db408b4ac329a8bbd81f
Daniel Berger <djberg96@gmail.com>
2014-11-14 08:52:21 -0700

    Use relative_require, include modules.

--- lib/windows/cng.rb
+++ lib/windows/cng.rb
@@ -1,6 +1,13 @@
-require 'ffi'
+require_relative 'cng/constants'
+require_relative 'cng/functions'
+require_relative 'cng/structs'
+require_relative 'cng/helper'
 
 module Windows
   class CNG
+    include Windows::CNGConstants
+    include Windows::CNGFunctions
+    include Windows::CNGStructs
+    include Windows::CNGHelper
   end
 end
