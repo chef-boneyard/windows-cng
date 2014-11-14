0437341ba8fd694afdc6134e3141a848d72d1097
Daniel Berger <djberg96@gmail.com>
2014-11-14 09:11:11 -0700

    Added initial constructor and destructor.

--- lib/windows/cng.rb
+++ lib/windows/cng.rb
@@ -9,5 +9,29 @@ module Windows
     include Windows::CNGFunctions
     include Windows::CNGStructs
     include Windows::CNGHelper
+
+    def initialize(algorithm = BCRYPT_SHA256_ALGORITHM, implementation = nil, flags = 0)
+      @algorithm = algorithm
+      @implementation = implementation
+      @flags = flags
+
+      @handle = FFI::MemoryPointer.new(:void)
+
+      if BCryptOpenAlgorithmProvider(@handle, @algorithm, @implementation, @flags) < 0
+        raise SystemCallError.new('BCryptOpenAlgorithmProvider', FFI.errno)
+      end
+    end
+
+    def close
+      if BCryptCloseAlgorithmProvider(@handle, 0) < 0
+        raise SystemCallError.new('BCryptCloseAlgorithmProvider', FFI.errno)
+      end
+    end
   end
+end
+
+if $0 == __FILE__
+  include Windows
+  cng = CNG.new
+  cng.close
 end
