16ef902bcc521c88cbee5173b0e7e7480ed93aa9
Daniel Berger <djberg96@gmail.com>
2014-11-14 10:19:30 -0700

    Modified constructor, started code for a #hash method.

--- lib/windows/cng.rb
+++ lib/windows/cng.rb
@@ -7,18 +7,47 @@ module Windows
   class CNG
     include Windows::CNGConstants
     include Windows::CNGFunctions
+    include Windows::MiscFunctions
     include Windows::CNGStructs
     include Windows::CNGHelper
 
     def initialize(algorithm = BCRYPT_SHA256_ALGORITHM, implementation = nil, flags = 0)
-      @algorithm = algorithm
-      @implementation = implementation
+      @algorithm = algorithm.wincode
+      @implementation = implementation ? implementation.wincode : implementation
       @flags = flags
 
-      @handle = FFI::MemoryPointer.new(:void)
+      @handle = FFI::MemoryPointer.new(:uintptr_t)
 
       if BCryptOpenAlgorithmProvider(@handle, @algorithm, @implementation, @flags) < 0
         raise SystemCallError.new('BCryptOpenAlgorithmProvider', FFI.errno)
+      end
+    end
+
+    def hash(data)
+      hash = FFI::MemoryPointer.new(:ulong)
+      result = FFI::MemoryPointer.new(:ulong)
+
+      status = BCryptGetProperty(
+        @handle,
+        BCRYPT_OBJECT_LENGTH.wincode,
+        hash,
+        hash.size,
+        result,
+        0
+      )
+
+      if status < 0
+        raise SystemCallError.new('BCryptGetProperty', status)
+      end
+
+      pbhash_object = HeapAlloc(GetProcessHeap(), 0, hash.read_ulong)
+
+      if pbhash_object.null?
+        raise SystemCallError.new('HeapAlloc', FFI.errno)
+      end
+
+      if pbhash_object && !pbhash_object.null?
+        HeapFree(GetProcessHeap(), 0, pbhash_object)
       end
     end
 
@@ -33,5 +62,6 @@ end
 if $0 == __FILE__
   include Windows
   cng = CNG.new
+  cng.hash("stuff")
   cng.close
 end
