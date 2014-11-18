234ffa94b49e820b38b0ac510ceec2b2b0c601a0
Daniel Berger <djberg96@gmail.com>
2014-11-18 16:40:43 -0700

    Created ntstatus typedef and use it.

--- lib/windows/cng/functions.rb
+++ lib/windows/cng/functions.rb
@@ -50,93 +50,95 @@ module Windows
     extend FFI::Library
     ffi_lib :bcrypt
 
+    typedef :long, :ntstatus
+
     ## CNG Cryptographic Primitive Functions
 
     attach_function :BCryptCloseAlgorithmProvider,
       [:pointer, :ulong],
-      :ulong
+      :ntstatus
 
     attach_function :BCryptCreateHash,
       [:pointer, :pointer, :pointer, :ulong, :pointer, :ulong, :ulong],
-      :ulong
+      :ntstatus
 
     attach_function :BCryptDecrypt,
       [:pointer, :pointer, :ulong, :pointer, :pointer, :ulong, :pointer, :ulong, :ulong, :ulong],
-      :ulong
+      :ntstatus
 
     attach_function :BCryptDeriveKey,
       [:pointer, :string, :pointer, :pointer, :ulong, :ulong, :ulong],
-      :ulong
+      :ntstatus
 
-    attach_function :BCryptDestroyHash, [:pointer], :ulong
-    attach_function :BCryptDestroyKey, [:pointer], :ulong
-    attach_function :BCryptDestroySecret, [:pointer], :ulong
+    attach_function :BCryptDestroyHash, [:pointer], :ntstatus
+    attach_function :BCryptDestroyKey, [:pointer], :ntstatus
+    attach_function :BCryptDestroySecret, [:pointer], :ntstatus
 
     attach_function :BCryptDuplicateHash,
       [:pointer, :pointer, :pointer, :ulong, :ulong],
-      :ulong
+      :ntstatus
 
     attach_function :BCryptDuplicateKey,
       [:pointer, :pointer, :pointer, :ulong, :ulong],
-      :ulong
+      :ntstatus
 
     attach_function :BCryptEncrypt,
       [:pointer, :pointer, :ulong, :pointer, :pointer, :ulong, :pointer, :ulong, :ulong, :ulong],
-      :ulong
+      :ntstatus
 
     attach_function :BCryptExportKey,
       [:pointer, :pointer, :string, :pointer, :ulong, :ulong, :ulong],
-      :ulong
+      :ntstatus
 
-    attach_function :BCryptFinalizeKeyPair, [:pointer, :ulong], :ulong
-    attach_function :BCryptFinishHash, [:pointer, :pointer, :ulong, :ulong], :ulong
+    attach_function :BCryptFinalizeKeyPair, [:pointer, :ulong], :ntstatus
+    attach_function :BCryptFinishHash, [:pointer, :pointer, :ulong, :ulong], :ntstatus
     attach_function :BCryptFreeBuffer, [:pointer], :void
-    attach_function :BCryptGenerateKeyPair, [:pointer, :pointer, :ulong, :ulong], :ulong
+    attach_function :BCryptGenerateKeyPair, [:pointer, :pointer, :ulong, :ulong], :ntstatus
 
     attach_function :BCryptGenerateSymmetricKey,
       [:pointer, :pointer, :pointer, :ulong, :pointer, :ulong, :ulong],
-      :ulong
+      :ntstatus
 
-    attach_function :BCryptGenRandom, [:pointer, :pointer, :ulong, :ulong], :ulong
+    attach_function :BCryptGenRandom, [:pointer, :pointer, :ulong, :ulong], :ntstatus
 
     attach_function :BCryptGetProperty,
       [:pointer, :buffer_in, :pointer, :ulong, :pointer, :ulong],
-      :ulong
+      :ntstatus
 
-    attach_function :BCryptHashData, [:pointer, :pointer, :ulong, :ulong], :ulong
+    attach_function :BCryptHashData, [:pointer, :pointer, :ulong, :ulong], :ntstatus
 
     attach_function :BCryptImportKey,
       [:pointer, :pointer, :string, :pointer, :pointer, :ulong, :pointer, :ulong, :ulong],
-      :ulong
+      :ntstatus
 
     attach_function :BCryptImportKeyPair,
       [:pointer, :pointer, :string, :pointer, :pointer, :ulong, :ulong],
-      :ulong
+      :ntstatus
 
     attach_function :BCryptOpenAlgorithmProvider,
       [:pointer, :buffer_in, :buffer_in, :ulong],
-      :ulong
+      :ntstatus
 
     attach_function :BCryptSecretAgreement,
       [:pointer, :pointer, :pointer, :ulong],
-      :ulong
+      :ntstatus
 
     attach_function :BCryptSetProperty,
       [:pointer, :string, :pointer, :ulong, :ulong],
-      :ulong
+      :ntstatus
 
     attach_function :BCryptSignHash,
       [:pointer, :pointer, :pointer, :ulong, :pointer, :ulong, :pointer, :ulong],
-      :ulong
+      :ntstatus
 
     attach_function :BCryptVerifySignature,
       [:pointer, :pointer, :pointer, :ulong, :pointer, :ulong, :ulong],
-      :ulong
+      :ntstatus
 
     begin
       attach_function :BCryptKeyDerivation,
         [:pointer, :pointer, :pointer, :ulong, :pointer, :ulong],
-        :ulong
+        :ntstatus
     rescue FFI::NotFoundError
       # Windows 8 or later
     end
